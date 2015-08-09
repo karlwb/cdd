e7d6636c917df228c9411d64c683a61ad8ec1e59
karlwb <karlwb@gmail.com>
2015-08-09 00:28:10 -0500

    Card improvements

--- lib/CDD/Card.pm
+++ lib/CDD/Card.pm
@@ -3,27 +3,69 @@ use Moo;
 
 has rank => (is => 'ro', 
              required => 1,
-             isa => sub{ die unless $_[0] =~ m/^[2-9JQKA]|10$/i },
+             isa => sub{ die "invalid rank $_[0]" unless $_[0] =~ m/^[2-9JQKA]|10$/i },
              coerce => sub { uc $_[0] },
             );
 has suit => (is => 'ro', 
              required=>1,
-             isa => sub{ die unless $_[0] =~ m/^[DCHS]$/i },
+             isa => sub{ die "invalid suit $_[0]" unless $_[0] =~ m/^[DCHS]$/i },
              coerce => sub { uc $_[0] },
             );
 has val  => (is => 'ro', 
              required=>1,
-             isa => sub { die if ($_[0] !~ m/^\d+$/ 
-                                  or $_[0] < 1 
-                                  or $_[0] > 52) 
+             isa => sub { die "val must be a number got $_[0]" if $_[0] !~ m/^\d+$/; 
+                          die "val must be positive got $_[0]" if $_[0] < 1;
+                          die "val must be <= 52 got $_[0]" if $_[0] > 52; 
                       },
             );
 
-use overload '""' => \&as_string;
+our %CARD_VAL = ( '3D'  => 1,  '3C'  => 2,  '3H'  => 3,  '3S'  => 4,
+                  '4D'  => 5,  '4C'  => 6,  '4H'  => 7,  '4S'  => 8,
+                  '5D'  => 9,  '5C'  => 10, '5H'  => 11, '5S'  => 12,
+                  '6D'  => 13, '6C'  => 14, '6H'  => 15, '6S'  => 16,
+                  '7D'  => 17, '7C'  => 18, '7H'  => 19, '7S'  => 20,
+                  '8D'  => 21, '8C'  => 22, '8H'  => 23, '8S'  => 24,
+                  '9D'  => 25, '9C'  => 26, '9H'  => 27, '9S'  => 28,
+                  '10D' => 29, '10C' => 30, '10H' => 31, '10S' => 32, 
+                  'JD'  => 33, 'JC'  => 34, 'JH'  => 35, 'JS'  => 36, 
+                  'QD'  => 37, 'QC'  => 38, 'QH'  => 39, 'QS'  => 40, 
+                  'KD'  => 41, 'KC'  => 42, 'KH'  => 43, 'KS'  => 44, 
+                  'AD'  => 45, 'AC'  => 46, 'AH'  => 47, 'AS'  => 48, 
+                  '2D'  => 49, '2C'  => 50, '2H'  => 51, '2S'  => 52,
+            );
+our %UNICODE = ( D => '♢', C => '♧', H => '♡', S => '♤' );
+
+sub BUILDARGS {
+    my ($class, @args) = @_;
+    die "Must supply args" if not defined $args[0];
+    if (ref($args[0]) eq '') {
+        if ($args[0] =~ m/^(.*)([DCHS])$/i) {
+            my $rank = uc $1;
+            my $suit = uc $2;
+              return {rank=>$rank, suit=>$suit, val=>$CARD_VAL{$rank.$suit}};
+        }
+    }
+    elsif (ref($args[0] eq 'HASH')) {
+        return $args[0];
+    }
+    return { @args };
+}
 
 sub as_string{ 
-    $_[0]->rank . $_[0]->suit 
+    $_[0]->rank . ($_[1] ? $UNICODE{$_[0]->suit} : $_[0]->suit);
 };
-    
+
+sub as_unicode {
+    shift->as_string(1);
+}
+
+sub _3way_compare { 
+    shift->val <=> shift->val;
+}
+
+use overload '""' => 'as_string';
+use overload '<=>' => '_3way_compare';
+use overload 'cmp' => '_3way_compare'; 
+
 1;
 
--- t/Card.t
+++ t/Card.t
@@ -2,26 +2,46 @@ use Modern::Perl;
 use Test::More;
 use Test::Exception;
 
+use constant CARD => sub { CDD::Card->new(@_) };
+
 require_ok('CDD::Card');
 use_ok('CDD::Card');
 dies_ok {CDD::Card->new()} 'new no args dies';
-dies_ok {CDD::Card->new(rank=>'A')} 'new rank only dies';
-dies_ok {CDD::Card->new(rank=>'A', suit=>'S')} 'new rank suit only dies';
-dies_ok {CDD::Card->new(rank=>11, suit=>'S', val=>1)} 'new bad rank';
-dies_ok {CDD::Card->new(rank=>10, suit=>'G', val=>1)} 'new bad suit';
-dies_ok {CDD::Card->new(rank=>10, suit=>'C', val=>0)} 'new bad val 1';
-dies_ok {CDD::Card->new(rank=>10, suit=>'C', val=>53)} 'new bad val 2';
-lives_ok {CDD::Card->new(rank=>'A', suit=>'S', val=>2)} 'new lives';
+dies_ok {CDD::Card->new(rank=>'A')} 'new full rank only dies';
+dies_ok {CDD::Card->new(rank=>'A', suit=>'S')} 'new full rank suit only dies';
+dies_ok {CDD::Card->new(rank=>11, suit=>'S', val=>1)} 'new full bad rank';
+dies_ok {CDD::Card->new(rank=>10, suit=>'G', val=>1)} 'new full bad suit';
+dies_ok {CDD::Card->new(rank=>10, suit=>'C', val=>0)} 'new full bad val 1';
+dies_ok {CDD::Card->new(rank=>10, suit=>'C', val=>53)} 'new full bad val 2';
+lives_ok {CDD::Card->new(rank=>'A', suit=>'S', val=>2)} 'new full lives';
+lives_ok {CDD::Card->new('3D')} 'new short lives';
 
-my $c = CDD::Card->new(rank=>'a', suit=>'s', val=>2);
-is $c->rank, 'A', 'get rank';
-is $c->suit, 'S', 'get suit';
-is $c->val, 2, 'get val';
-is "$c", "AS", 'string context';
-is $c->as_string, 'AS', 'as_string';
+my $ace = CDD::Card->new('as');
+is $ace->rank, 'A', 'get rank ace';
+is $ace->suit, 'S', 'get suit spades';
+is $ace->val, 48, 'get val';
+is "$ace", "AS", 'string context';
+is $ace->as_string, 'AS', 'as_string';
+is $ace->as_unicode, 'A♤', 'as_unicode';
+dies_ok {$ace->rank(4)} 'set rank dies';
+dies_ok {$ace->suit('C')} 'set suit dies';
+dies_ok {$ace->val(2)} 'set val dies';
 
-dies_ok {$c->rank(4)} 'set rank dies';
-dies_ok {$c->suit('C')} 'set suit dies';
-dies_ok {$c->val(2)} 'set val dies';
+my $six = CDD::Card->new('6H');
+my $six2 = CDD::Card->new('6h');
+
+cmp_ok $six, '<', $ace, '6H < AS';
+cmp_ok $six, '<=', $six2, '6H <= 6h';
+cmp_ok $six, '==', $six2, '6H == 6h';
+cmp_ok $ace, '>=', $six, 'AS >= 6H';
+cmp_ok $ace, '>', $six, 'AS > 6H';
+
+cmp_ok $six, 'lt', $ace, '6H lt AS';
+cmp_ok $six, 'le', $six2, '6H le 6h';
+cmp_ok $six, 'eq', $six2, '6H eq 6h';
+cmp_ok $ace, 'ge', $six, 'AS ge 6H';
+cmp_ok $ace, 'gt', $six, 'AS gt 6H';
+
+
 
 done_testing;
