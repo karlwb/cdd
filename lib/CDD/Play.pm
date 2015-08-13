5ede07ca514aef662c73929a1ad370e224319810
karlwb <karlwb@gmail.com>
2015-08-13 00:42:34 -0500

    tweak Play to work with subgroups like fullhouse and quad+1

--- lib/CDD/Play.pm
+++ lib/CDD/Play.pm
@@ -8,17 +8,45 @@ with 'CDD::Group';
 has size  => ( is => 'ro');
 has val   => ( is => 'ro');
 has highest => ( is => 'ro' );
-            
+has subgroup => ( is => 'ro' );
+
 sub BUILDARGS {
     my ($class, @args) = @_;
     my $group = CDD::SimpleGroup->new(@args);
-    my $cards = $class->_validate($group);
-    my $val   = $class->_valuate($group);
-    my $highest = $class->_highest($group);
+    my $validate = $class->_validate($group);
+    my $cards;
+    my $subgroup;
+    my $valuate;
+    if ( ref($validate->[0]) eq 'CDD::Card') {
+        # non fullhouse, non quad+1 case
+        $cards = $validate;
+        $valuate = $cards;
+    }
+    else {
+        # full house
+        if(@{$validate->[0]} == 3) {
+            $subgroup = { triple => $validate->[0],
+                          pair   => $validate->[1],
+                         };
+            $valuate = $subgroup->{triple};
+        }
+        # quad+1
+        else {
+            $subgroup = { quad => $validate->[0],
+                          single => $validate->[1],
+                       };
+            $valuate = $subgroup->{quad};
+        }
+        $cards  = [@{$validate->[0]}, @{$validate->[1]}];
+    }
+    $valuate = CDD::SimpleGroup->new($valuate);
+    my $val   = $class->_valuate($valuate);
+    my $highest = $class->_highest($valuate);
     return { cards=>$cards, 
              val=>$val, 
              size=>scalar(@{$cards}), 
-             highest=>$highest 
+             highest=>$highest,
+             subgroup=>$subgroup,
            };
 }
 
