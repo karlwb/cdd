c32aabebef6d1f17573b836af6d9c7a791a01bf4
karlwb <karlwb@gmail.com>
2015-08-19 20:46:58 -0500

    Use card ranks and suits in deck

--- lib/CDD/Deck.pm
+++ lib/CDD/Deck.pm
@@ -7,8 +7,8 @@ with 'CDD::Group';
 sub _build_cards {
     my @cards = ();
     my $val   = 1;
-    for my $rank (qw/3 4 5 6 7 8 9 10 J Q K A 2/) {
-        for my $suit (qw/D C H S/) {
+    for my $rank (@CDD::Card::RANKS) {
+        for my $suit (@CDD::Card::SUITS) {
             push @cards, CDD::Card->new(rank => $rank, suit => $suit, val => $val++);
         }
     }
